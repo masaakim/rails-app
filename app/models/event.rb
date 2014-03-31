9feb9201474814b4ee06e467bbdc37d287e3ea5e
Masaaki Morishita <ikasama3.0@gmail.com>
2014-03-31 12:58:47 +0900

    add validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ActiveRecord::Base
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :place, presence: true
+  validates :place_url, presence: true
+  validates :staff_count, presence: true
+  validates :is_closed, presence: true
 end
