9084e81f369d25d562ca58169cb8e397881abde1
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-09 18:26:26 +0900

    validate lesson

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :event
+  has_many :lesson
 
   validates :name, presence: true
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,7 @@
 class Lesson < ActiveRecord::Base
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :pdf_file, presence: true
 end
