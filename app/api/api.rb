d37f3177afe6c52a72c05babb3ad8149cd99c420
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-13 16:57:22 +0900

    delete helper method

--- app/api/api.rb
+++ app/api/api.rb
@@ -4,16 +4,6 @@ module API
     version 'v1', using: :path
     format :json
 
-    helpers do
-      def current_user
-        @current_user ||= User.authorize!(env)
-      end
-
-      def authenticate!
-        error!('401 Unauthorized', 401) unless current_user
-      end
-    end
-
     mount Category_API
     mount Location_API
     mount Principal_API
