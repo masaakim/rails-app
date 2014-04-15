1fc0dfe7b402e8bd747f8bb0738060885ca7b670
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-15 16:14:53 +0900

    fix event model validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,11 +5,9 @@ class Event < ActiveRecord::Base
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :url, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   validates :place, presence: true
-  validates :place_url, presence: true
   validates :staff_count, presence: true
   validates :is_closed, presence: true
 end
