dd2f99cf629b2c6baf010a42420f6b875094deac
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-15 16:13:45 +0900

    fix event model validation

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without name"
+  it "is invalid without description"
+  it "is invalid without start_time"
+  it "is invalid without end_time"
+  it "is invalid without place"
+  it "is invalid without staff_count"
+  it "is invalid without is_closed"
 end
